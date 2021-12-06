bc8e45fee82578daa7bff36c2e99c84f00a332e4
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2021-12-06 21:30:42 +0000

    Basic Example for Typescript

--- index.ts
+++ index.ts
@@ -1,1 +1,7 @@
-console.log('Hello TS')
+function greetUser(greeting: string = 'Hello', name: string = 'User'): string {
+    return `${greeting}, ${name}`
+}
+
+const h1Node = document.getElementById('greeting') as HTMLHeadingElement
+
+h1Node.textContent = greetUser('Hello', 'Vijay')
